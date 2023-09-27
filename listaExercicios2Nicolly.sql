d3fa14f344dd2b4c97abde3eebeabd1b6a5d4ec5
Nicolly <nicollycarmo840@gmail.com>
2023-09-26 22:43:49 -0300

    segundo exercicio finalizado

--- listaExercicios2Nicolly.sql
+++ listaExercicios2Nicolly.sql
@@ -10,3 +10,20 @@ END;
 DELIMITER ;
 CALL sp_ListarAutores();
 
+DELIMITER //
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoria_nome VARCHAR(200))
+BEGIN
+    SELECT Livro.Titulo
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID WHERE Categoria.Nome = categoria_nome
+END;
+//
+
+DELIMITER ;
+
+CALL sp_LivrosPorCategoria('Romance');
+CALL sp_LivrosPorCategoria('Ciência');
+CALL sp_LivrosPorCategoria('Ficção Científica');
+CALL sp_LivrosPorCategoria('História');
+CALL sp_LivrosPorCategoria('Autoajuda');
+
