7adb42ee2a4f8f835dcdb58edb216746d803408b
Nicolly <nicollycarmo840@gmail.com>
2023-09-26 22:44:25 -0300

    terceiro exercicio completo

--- listaExercicios2Nicolly.sql
+++ listaExercicios2Nicolly.sql
@@ -27,3 +27,22 @@ CALL sp_LivrosPorCategoria('Ficção Científica');
 CALL sp_LivrosPorCategoria('História');
 CALL sp_LivrosPorCategoria('Autoajuda');
 
+DELIMITER //
+
+CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN categoria_nome VARCHAR(200), OUT total_livros INT)
+BEGIN
+    SELECT COUNT(*) INTO total_livros
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoria_nome;
+END;
+//
+
+DELIMITER ;
+
+CALL sp_ContarLivrosPorCategoria('Romance', @total_livros);
+CALL sp_ContarLivrosPorCategoria('Ciência', @total_livros);
+CALL sp_ContarLivrosPorCategoria('Ficção Científica', @total_livros);
+CALL sp_ContarLivrosPorCategoria('História', @total_livros);
+CALL sp_ContarLivrosPorCategoria('Autoajuda', @total_livros);
+
